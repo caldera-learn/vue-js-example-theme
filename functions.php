539fd5cbf53c6681697c7090f1a2cd70b41bfef2
Josh Pollock <Josh@JoshPress.net>
2016-12-31 19:58:21 -0500

    router

--- functions.php
+++ functions.php
@@ -1,21 +1,20 @@
 <?php
 
 
+add_action( 'wp_enqueue_scripts', function () {
+	wp_enqueue_script( 'jquery' );
+	wp_enqueue_script( 'vue-router', get_template_directory_uri() . '/assets/js/vue-router.js', [ 'vuejs' ] );
+	wp_enqueue_script( 'vuejs', get_template_directory_uri() . '/assets/js/vue.js' );
+} );
 
-add_action( 'wp_enqueue_scripts', function(){
-
-    wp_enqueue_script( 'jquery' );
-   wp_enqueue_script( 'vuejs', get_template_directory_uri() . '/assets/js/vue.js' );
-});
-
-add_action('init', function () {
-    global $wp_post_types;
-    foreach (get_post_types() as $post_type) {
-        if ($wp_post_types[$post_type]->public && !$wp_post_types[$post_type]->show_in_rest) {
-            $wp_post_types[$post_type]->show_in_rest = true;
-            if (empty($wp_post_types[$post_type]->rest_base)) {
-                $wp_post_types[$post_type]->rest_base = $wp_post_types[$post_type]->name;
-            }
-        }
-    }
-}, 40000);
+add_action( 'init', function () {
+	global $wp_post_types;
+	foreach ( get_post_types() as $post_type ) {
+		if ( $wp_post_types[ $post_type ]->public && ! $wp_post_types[ $post_type ]->show_in_rest ) {
+			$wp_post_types[ $post_type ]->show_in_rest = true;
+			if ( empty( $wp_post_types[ $post_type ]->rest_base ) ) {
+				$wp_post_types[ $post_type ]->rest_base = $wp_post_types[ $post_type ]->name;
+			}
+		}
+	}
+}, 40000 );
--- index.php
+++ index.php
@@ -5,32 +5,48 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-            <div id="app">
-                <post-list></post-list>
-            </div>
+                <div id="app">
+                    <p>
+                        <router-link to="/">Home</router-link>
+                        <router-link to="/posts">Posts</router-link>
+                    </p>
+                    <router-view></router-view>
+                </div>
 
-            <script type="text/html" id="post-list-tmpl">
-                <div id="posts">
-                    <div v-for="post in posts">
+                <script type="text/html" id="post-list-tmpl">
+                    <div id="posts">
+                        <div v-for="post in posts">
+                            <article v-bind:id="'post-' + post.id">
+                                <header>
+                                    <h2 class="post-title">
+                                        {{post.title.rendered}}
+                                    </h2>
+                                </header>
+                                <div class="entry-content" v-html="post.excerpt.rendered"></div>
+                                <router-link :to="{ name: 'post', params: { id: post.id }}">
+                                    Read More
+                                </router-link>
+                            </article>
+                        </div>
+                    </div>
+                </script>
+
+                <script type="text/html" id="post-tmpl">
+                    <div class="post">
+
                         <article v-bind:id="'post-' + post.id">
                             <header>
                                 <h2 class="post-title">
-                                    {{post.title.rendered}}
+                                    {{post.title}}
                                 </h2>
                             </header>
-                            <div class="entry-content" v-html="post.excerpt.rendered" v-if="list"></div>
-                            <div class="entry-content" v-html="post.content.rendered" v-if="post.id == show"></div>
-                            <a href="#" class="close" role="button" v-on:click.stop="close" v-if="post.id == show">Close</a>
+                            <div class="entry-content" v-html="post.content"></div>
 
-                            <a href="#" class="read-more" role="button" v-on:click.stop="read(post.id)" v-if="list">Read More</a>
                         </article>
                     </div>
-                </div>
-            </script>
+                </script>
 
-
-
-            <script>
+                <script>
                 (function($){
                     var config = {
                         api: {
@@ -39,48 +55,70 @@ get_header(); ?>
                         nonce: "<?php echo wp_create_nonce( 'wp_rest' ); ?>"
                     };
 
-                    var vue;
-                    $.when( $.get( config.api.posts + '1' ) ).then( function( d ){
-                        var posts = Vue.component('post-list', {
-                            template: '#post-list-tmpl',
-                            data: function () {
-                                return {
-                                    list: true,
-                                    show: 0,
-                                    posts: d
-                                }
+                    var posts = Vue.component('post-list', {
+                        template: '#post-list-tmpl',
+                        data: function() {
+                            return{
+                                posts: []
+                            }
+                        },
+                        mounted: function () {
+                            this.getPosts();
+                        },
+                        methods: {
+                            getPosts: function () {
+                                var self = this;
+                                $.get( config.api.posts, function( r ){
+                                    self.$set( self, 'posts', r );
+                                });
+
                             },
-                            methods: {
-                                read: function( id ){
-                                    this.list = false;
-                                    this.show = id;
-
-                                },
-                                close: function(){
-                                    this.list = true;
-                                    this.show = 0;
-                                }
-                            }
-                        });
-
-                        var post = Vue.component( 'post', {
-                           template: '#post-tmpl',
-                            props: function(){
-                               return {
-                                   post: Object
-                               }
-                            }
-                        });
-
-                        vue = new Vue({
-                            el: '#app',
-                            data: {}
-
-                        });
+                        }
                     });
+
+                    var post = Vue.component( 'post', {
+                        template: '#post-tmpl',
+                        data: function() {
+                            return{
+                                post: []
+                            }
+                        },
+                        mounted: function () {
+                            this.getPost();
+                        },
+                        methods: {
+                            getPost: function(){
+                               var self = this;
+                               $.get( config.api.posts +  self.$route.params.id, function(r){
+                                   r.title = r.title.rendered;
+                                   r.content = r.content.rendered;
+                                   self.$set( self, 'post', r );
+                                });
+                            }
+                        }
+                    });
+
+
+                    var router = new VueRouter({
+                        mode: 'history',
+                        routes: [
+                            { path: '/', name: 'home', template: '<div>Hi Roy.</div>' },
+                            { path: '/posts', name: 'posts', component: posts },
+                            { path: '/posts/:id', name: 'post', component: post },
+                        ]
+                    });
+
+
+                    new Vue({
+                        router,
+                    }).$mount('#app')
+
+
+
                 })( jQuery );
 
             </script>
+
 
 		</main><!-- .site-main -->
 	</div><!-- .content-area -->
